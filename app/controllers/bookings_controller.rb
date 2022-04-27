19bf54436eb9075cf2720d5d15a12ab95abdf96c
xiongde shen <xiongde.shen@outlook.com>
2022-04-27 15:15:07 +0800

    add_create_booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,5 +4,17 @@ class BookingsController < ApplicationController
         @bookings = user.bookings
     end
     
-    
+    def create
+    @booking = Booking.new(booking_params)
+    @booking.save
+
+    # no need for app/views/restaurants/create.html.erb
+    redirect_to booking_path(@booking)
+    end
+
+    private
+
+    def booking_params
+        params.require(:booking).permit(:user_id, :item_id, :booking_start, :booking_end)
+    end
 end
