a035feb0e99dfa96f2c24b15daf5af1c9a0b2d1a
xiongde shen <xiongde.shen@outlook.com>
2022-04-28 17:04:14 +0800

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,12 @@ Rails.application.routes.draw do
   # root to: "users#home"oooh
   resources :items do
     resources :bookings, :only => [:new, :create]
-
   end
+
+  resources :bookings do
+    resources :reviews, only: [ :new, :create ]
+  end
+  
   resources :bookings, :only => :index
-  # get  "items",     to: "items#index"
-  # get  "items/new", to: "items#new"
-  # post "items",     to: "items#create"
-  # get "items/:id/edit",     to: "items#edit"
-  # post "items/:id/bookings", to:"bookings#create"
-  # get  "bookings",  to: "bookings#index"
+
 end
