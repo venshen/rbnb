f42a0cf90c2871417032bec33b09877b62399afc
xiongde shen <xiongde.shen@outlook.com>
2022-04-28 15:58:05 +0800

    add_booking_index_routes_and_finshed_how_to_book_an_item_feature

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # root to: "users#home"oooh
   resources :items do
-    resources :bookings, only: [ :new, :create ]
-  end
-  resources :bookings do
-    resources :reviews, only: [ :new, :create ]
+    resources :bookings, :only => [:new, :create]
 
   end
-
+  resources :bookings, :only => :index
   # get  "items",     to: "items#index"
   # get  "items/new", to: "items#new"
   # post "items",     to: "items#create"
